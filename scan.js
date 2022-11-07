23ba8a77a3e13c8e06dd95594b3ac2b32f0347e3
DelroyGayle <delroygayle01@gmail.com>
2022-11-07 13:37:41 +0000

    initial commit

---
+++ scan.js
@@ -0,0 +1,11 @@
+/****************************************************/
+/* File: scan.js                                    */
+/* The scanner interface for the TINY compiler      */
+/* Compiler Construction: Principles and Practice   */
+/* Kenneth C. Louden                                */
+/****************************************************/
+
+
+/* MAXTOKENLEN is the maximum size of a token */
+const MAXTOKENLEN = 40;
+
